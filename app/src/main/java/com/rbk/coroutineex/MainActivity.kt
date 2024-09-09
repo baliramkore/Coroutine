781200c20f6fb76b2522395533c1ab6559e44327
baliram <baliramkore@yahoo.com>
2024-09-09 12:39:12 +0530

    Applied CoroutineScope and execution will be async will not affect UI Thread

--- app/src/main/java/com/rbk/coroutineex/MainActivity.kt
+++ app/src/main/java/com/rbk/coroutineex/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.rbk.coroutineex
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var counter = 0
+        var btn: Button = findViewById(R.id.btnAdd)
+        var textView: TextView = findViewById(R.id.text_view)
+
+
+        btn.setOnClickListener() {
+            counter++
+            textView.text = counter.toString()
+
+        }
+
+       CoroutineScope(Dispatchers.IO).launch {
+
+           downloadBigFile()
+       }
+    }
+
+    private fun downloadBigFile() {
+        for (i in 1..100000){
+            Log.d("TAG","Downloading: $i")
         }
     }
 }
